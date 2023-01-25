b4826309e400bea733fc3e61f6c2ed81851fbda7
Francois Bornais <francois.bornais-doucet@lacapitale.com>
2023-01-24 19:02:37 -0500

    adjust to skeena region

--- data-raw/dem.R
+++ data-raw/dem.R
@@ -1,7 +1,15 @@
 # Digital elevation model
 # remotes::install_github("bcgov/bcmaps")
 library(bcmaps)
+
+
+
+#Skeena Region AOI
+nr <- bcmaps::nr_regions()
+skeena_aoi <- nr$geometry[which(nr$ORG_UNIT == "RSK")]
+
+
 # This will cache the whole cded locally. It's a large download.
 # For development purpose only, we will compress it a later stage
 # for better efficiency.
-dem <- cded(aoi = bc_bbox())
+dem <- cded(aoi = sf::st_bbox(skeena_aoi))

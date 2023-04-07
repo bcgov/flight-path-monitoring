643df66d44421c68140ffd296fc1573ce82369d7
Bruno Tremblay <bruno@boostao.ca>
2023-04-07 12:18:46 -0400

    works fine, great start, building from there

--- app/app.R
+++ app/app.R
@@ -5,14 +5,7 @@ library(flight.path.monitoring)
 library(leaflet)
 
 options(shiny.maxRequestSize = 10 * 1024^2)
-
-flightPathDir <- file.path("data-raw", "Heli data", "NEH", "2021")
-habitat_areas <- readRDS(file.path("data-raw", "Habitat", "habitat_areas.rds"))
-wildlife_telemetry <- readRDS(file.path("data-raw", "Habitat", "wildlife_telemetry.rds"))
-
-incursion_zones_distance <- distances(High = 1500, Moderate = 1000, Low = 500, reflabel = "In UWR")
-
-
+flightPathDir <- file.path("..", "data-raw", "Heli data", "NEH", "2021")
 
 ui <- dashboardPage(
   dashboardHeader(title = 'Flight Path Monitoring'),
@@ -68,32 +61,18 @@ server <- function(input, output) {
 
   processed_flight <- reactive({
     withProgress(message = "Processing Flight", value = 0, {
-      process_flight(flight = flight(),
-                     zones = habitat_areas,
-                    dist = distances(low = 1500, moderate = 1000, high = 500, reflabel = "in_UWR"),
-                    geom_out = TRUE)
+      process_flight(flight = flight(), geom_out = TRUE)
       })
     }) |> bindCache(list.files(flightPathDir, full.names = TRUE)[flight_row()])
 
   output$flight_map <- renderLeaflet({
-    leaflet() |>
-      addProviderTiles(provider = "Esri.WorldTopoMap") |>
-      addPolygons(data = processed_flight()[["zones"]][["in_UWR"]], color = "white", opacity = 1, weight = 1, fillColor = "#db0f27", fillOpacity = 0.35) |>
-      addPolygons(data = processed_flight()[["zones"]][["high"]], color = "white", opacity = 1, weight = 1, fillColor = "#db0f27", fillOpacity = 0.275) |>
-      addPolygons(data = processed_flight()[["zones"]][["moderate"]], color = "white", opacity = 1, weight = 1, fillColor = "#db0f27", fillOpacity = 0.2) |>
-      addPolygons(data = processed_flight()[["zones"]][["low"]], color = "white", opacity = 1, weight = 1, fillColor = "#db0f27", fillOpacity = 0.125) |>
-      addPolylines(data = processed_flight()[["flight"]], weight = 1, color = "darkgreen", dashArray = 4) |>
-      addPolylines(data = processed_flight()[["segments"]][["in_UWR"]], weight = 2, color = "darkblue", opacity = 1) |>
-      addPolylines(data = processed_flight()[["segments"]][["high"]], weight = 2, color = "blue", opacity = 1) |>
-      addPolylines(data = processed_flight()[["segments"]][["moderate"]], weight = 2, color = "cornflowerblue", opacity = 1) |>
-      addPolylines(data = processed_flight()[["segments"]][["low"]], weight = 2, color = "skyblue", opacity = 1) |>
-      addPolylines(data = processed_flight()[["segments"]][["filtered"]], weight = 2, color = "deeppink", opacity = 1)
+    plot(processed_flight())
   })
 
   #output$flight_stats <- DT::renderDT(processed_flight()[["summary"]])
 
   output$flight_name <- renderValueBox(valueBox(processed_flight()[["summary"]][["name"]], subtitle = "Flight Name"))
-  output$flight_time_uwr <- renderValueBox(valueBox(value = round(processed_flight()[["summary"]][["in_UWR"]], 2),
+  output$flight_time_uwr <- renderValueBox(valueBox(value = round(processed_flight()[["summary"]][["in_uwr"]], 2),
                                                     subtitle = "In UWR", color = "red"))
   output$flight_time_high <- renderValueBox(valueBox(value = round(processed_flight()[["summary"]][["high"]], 2),
                                                      subtitle = "High", color = "orange"))
